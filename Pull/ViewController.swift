e83dc9c27caaca5bc62d749e0e546d94bb04180b
srtek <mac@srteks-mac-mini.local>
2018-11-27 12:38:00 +0530

    changes by raghav

--- Pull/ViewController.swift
+++ Pull/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
        
         print("ready for commit 1")
         print("changes done by other developer. need to pull before starting work")
+        print("changes by raghav")
     }
 
 }
